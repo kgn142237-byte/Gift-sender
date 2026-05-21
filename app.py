f081737b4bc0b085a0a157060d620c265f67d1df
kgn142237-byte <kgn142237@gmail.com>
2026-05-21 16:06:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -159,4 +159,5 @@ def send_gift():
     except Exception as e: return jsonify({"success": False, "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
